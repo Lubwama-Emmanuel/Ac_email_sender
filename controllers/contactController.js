b1a925017e7671dcdaf9e85c1d1350083eccf895
Rex <lubwamaemmanuel1@gmail.com>
2023-08-09 16:44:39 +0300

    refactor:a little clean up

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -6,6 +6,8 @@ exports.sendContact = async (req, res) => {
 
     await new Email(information).sendContact();
 
+    console.log(information);
+
     res.status(200).json({
       status: "Success",
       data: {
@@ -13,6 +15,7 @@ exports.sendContact = async (req, res) => {
       },
     });
   } catch (error) {
+    console.log("Error", error);
     res.status(400).json({
       status: "Failed",
       err: {
