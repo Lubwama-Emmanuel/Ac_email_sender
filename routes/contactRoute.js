4a76eec04b0638fa372031033f69c4c8216c2507
Rex <lubwamaemmanuel1@gmail.com>
2023-09-03 22:31:57 +0300

    feat:(added untangled connect route)

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const contactController = require("../controllers/contactController");
 
 router.post("/newContact", contactController.sendContact);
+router.post("/connect", contactController.sendConnect);
 
 module.exports = router;
