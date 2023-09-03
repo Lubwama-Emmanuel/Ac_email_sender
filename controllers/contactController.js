5ddd3d48ffa3f29a7614459da6f1108146261f78
Rex <lubwamaemmanuel1@gmail.com>
2023-09-03 22:30:40 +0300

    feat:(added controller for untangled site)

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -6,7 +6,28 @@ exports.sendContact = async (req, res) => {
 
     await new Email(information).sendContact();
 
-    console.log(information);
+    res.status(200).json({
+      status: "Success",
+      data: {
+        information,
+      },
+    });
+  } catch (error) {
+    console.log("Error", error);
+    res.status(400).json({
+      status: "Failed",
+      err: {
+        error,
+      },
+    });
+  }
+};
+
+exports.sendConnect = async (req, res) => {
+  try {
+    const information = req.body;
+
+    await new Email(information).sendConnect();
 
     res.status(200).json({
       status: "Success",
